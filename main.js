59920b970dd7f7d18e3cfe45ae0728bdac14d806
daeru-ne <ando.daryl@hotmail.com>
2021-08-08 21:49:27 +0100

    added basic html, css, js; added basic approach;

---
+++ main.js
@@ -0,0 +1,37 @@
+// /* // comment box
+// */ //
+
+// /* // operational
+// */ //
+
+// /* // diagnostic
+// */ //
+
+/* // approach
+operate(operator, numA, numB)
+  switch(operator)
+    add
+    subtract
+    multiply
+    divide
+  update display
+buttons
+  operators
+    add
+    subtract
+    multiply
+    divide
+    equals
+  other
+    numbers
+    decimal point
+    backspace
+    clear: wipe all data
+note
+  current answer = numA
+  pressing numbers = numberString
+  calculating = parseFloat(numberString)
+  divide by zero = funny message
+  decimal point disabled after use
+
+// */ //
