a5d9cf63c6f9d3fcf48b2632ff677f5caa460195
daeru-ne <ando.daryl@hotmail.com>
2021-08-09 20:13:48 +0100

    updated approach; updated diagnostic 1;

--- main.js
+++ main.js
@@ -46,10 +46,10 @@ shift: toggles remove button -- delete / clear
 remove: delete: delete last char / clear: reset display
 
 -- idea 2 --
-numbersMode: enter firstNumbers
-operatorsMode: store firstNumbers, display firstNumbers + operator
-numbersMode: enter secondNumbers
-operatorsMode: if (firstNumbers stored ) store secondNumbers, always activate = operator, display results, append non = operator
+numbersMode: enter firstNumbers, update display-content
+operatorsMode: store firstNumbers, display-memory firstNumbers + operator
+numbersMode: enter secondNumbers, update display
+operatorsMode: if (firstNumbers stored ) store secondNumbers, always activate = operator, display-content results, append non = operator
 // */ //
 
 // /* // diagnostic
@@ -57,6 +57,22 @@ const meta = {
   elems: {
     displayContent: document.getElementById("display-content"),
     memoryContent: document.getElementById("memory-content"),
+    operators: {
+      add: document.getElementById("add"),
+      subtract: document.getElementById("subtract"),
+      divide: document.getElementById("divide"),
+      multiply: document.getElementById("multiply"),
+      equals: document.getElementById("equals"),
+    },
+    mechanics: {
+      decimal: document.getElementById("decimal"),
+      shift: document.getElementById("shift"),
+      remove: document.getElementById("remove"),
+    },
+    config: {
+      shiftToggle: false,
+      removeMode: { clear: "clear", delete: "delete" },
+    },
   },
 };
 const methods = {
@@ -93,4 +109,41 @@ const methods = {
     return { operator: operator[0], numA: numbers[0], numB: numbers[1] };
   },
 };
+
+const enterNumbers = (num) => {
+  updateDisplay(num.toString());
+};
+
+const enterOperator = (operator) => {
+  const opList = meta.elems.operators;
+  switch (operator) {
+    case opList.add:
+      break;
+    case opList.subtract:
+      break;
+    case opList.divide:
+      break;
+    case opList.multiply:
+      break;
+    case opList.equals:
+      break;
+    default:
+      console.error("operator not found");
+  }
+};
+
+const enterMechanics = (mechanic) => {
+  const mechList = meta.elems.mechanics;
+  switch (mechanic) {
+    case mechList.decimal:
+      break;
+    case mechList.shift:
+      break;
+    case mechList.remove:
+      break;
+    default:
+      console.error("mechanic not found");
+  }
+};
+
 // */ //
