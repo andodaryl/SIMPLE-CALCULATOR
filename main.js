be53159c38b3a20887c42a9598b762ae12d0aa04
daeru-ne <ando.daryl@hotmail.com>
2021-08-09 16:52:49 +0100

    added logic process map .drawio

--- main.js
+++ main.js
@@ -5,6 +5,44 @@
 // */ //
 
 // /* // diagnostic
+const meta = {
+  origin: { displayValue: 0 },
+  elems: {
+    displayContent: document.getElementById("display-content"),
+  },
+};
+const methods = {
+  resetDisplay() {
+    elems.displayContent.innerText = meta.origin.displayValue;
+  },
+  updateDisplay(str) {
+    elems.displayContent.innerText += str;
+  },
+  operate(operator, numA, numB) {
+    switch (operator) {
+      case "add":
+        return numA + numB;
+        break;
+      case "subtract":
+        return numA - numB;
+        break;
+      case "multiply":
+        return numA * numB;
+        break;
+      case "divide":
+        return numA / numB;
+        break;
+      default:
+        console.error("Operate(operator, numA, numB) invalid operator");
+    }
+  },
+  evaluate(str) {
+    const numbers = str.match(/[-+]?([0-9]*[.])?[0-9]+[e]?/gi);
+    const operator = str.match(/[-+x/]/gi);
+    return { operator: operator[0], numA: numbers[0], numB: numbers[1] };
+  },
+};
+
 // */ //
 
 /* // approach
@@ -34,4 +72,17 @@ note
   divide by zero = funny message
   decimal point disabled after use
 
+display: original is "0"
+numbers
+  update display.innerText with numbers
+operators
+  first press: update display.innerText
+  second press: 
+    if no operator, change to recently pressed operator
+    activates equal sign and update display.innerText
+  equal sign: takes display.innerText and evaluates numA..operator..numB
+mechanics
+  decimal: update display.innerText and disable button
+  shift: toggles remove button -- delete / clear
+  remove: delete: delete last char / clear: reset display
 // */ //
